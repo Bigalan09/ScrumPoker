692eb2a1f82e61f0a38255e2c9294878c70a9945
Alan <bigalan09@gmail.com>
2021-06-04 06:57:05 +0100

    fix

--- scrum/src/pages/s-dashboard/s-dashboard.tsx
+++ scrum/src/pages/s-dashboard/s-dashboard.tsx
@@ -153,6 +153,7 @@ export class SDashboard {
   }
 
   fs_listen_online() {
+    var self = this;
     this.firebase.firestore().collection('status')
       .where('state', '==', 'online')
       .onSnapshot(function (snapshot) {
@@ -160,12 +161,12 @@ export class SDashboard {
           if (change.type === 'added') {
             var msg = 'User ' + change.doc.id + ' is online.';
             console.log(msg);
-            this.msghistory.push(msg);
+            self.msghistory.push(msg);
           }
           if (change.type === 'removed') {
             var msg = 'User ' + change.doc.id + ' is offline.';
             console.log(msg);
-            this.msghistory.push(msg);
+            self.msghistory.push(msg);
           }
         });
       });
