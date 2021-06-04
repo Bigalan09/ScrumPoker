75bb802126c66584f2515a7d14e756247e3a1aa3
Alan <bigalan09@gmail.com>
2021-06-04 06:41:31 +0100

    Presence

--- scrum/src/pages/s-dashboard/s-dashboard.tsx
+++ scrum/src/pages/s-dashboard/s-dashboard.tsx
@@ -12,7 +12,7 @@ export class SDashboard {
   @State() roomId: string = '';
   @State() userId: string = '';
   @State() db: any;
-  @State() history: string[] = [];
+  @State() msghistory: any[] = [];
 
   @Prop() firebase: any;
 
@@ -85,17 +85,9 @@ export class SDashboard {
   }
 
   rtdb_presence() {
-    // [START rtdb_presence]
-    // Fetch the current user's ID from Firebase Authentication.
     var uid = this.firebase.auth().currentUser.uid;
-
-    // Create a reference to this user's specific status node.
-    // This is where we will store data about being online/offline.
     var userStatusDatabaseRef = this.firebase.database().ref('/status/' + uid);
 
-    // We'll create two constants which we will write to 
-    // the Realtime database when this device is offline
-    // or online.
     var isOfflineForDatabase = {
       state: 'offline',
       last_changed: this.firebase.database.ServerValue.TIMESTAMP,
@@ -106,36 +98,18 @@ export class SDashboard {
       last_changed: this.firebase.database.ServerValue.TIMESTAMP,
     };
 
-    // Create a reference to the special '.info/connected' path in 
-    // Realtime Database. This path returns `true` when connected
-    // and `false` when disconnected.
     this.firebase.database().ref('.info/connected').on('value', function (snapshot) {
-      // If we're not currently connected, don't do anything.
       if (snapshot.val() == false) {
         return;
       };
 
-      // If we are currently connected, then use the 'onDisconnect()' 
-      // method to add a set which will only trigger once this 
-      // client has disconnected by closing the app, 
-      // losing internet, or any other means.
       userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase).then(function () {
-        // The promise returned from .onDisconnect().set() will
-        // resolve as soon as the server acknowledges the onDisconnect() 
-        // request, NOT once we've actually disconnected:
-        // https://firebase.google.com/docs/reference/js/firebase.database.OnDisconnect
-
-        // We can now safely set ourselves as 'online' knowing that the
-        // server will mark us as offline once we lose connection.
         userStatusDatabaseRef.set(isOnlineForDatabase);
       });
     });
-    // [END rtdb_presence]
   }
 
   rtdb_and_local_fs_presence() {
-    // [START rtdb_and_local_fs_presence]
-    // [START_EXCLUDE]
     var uid = this.firebase.auth().currentUser.uid;
     var userStatusDatabaseRef = this.firebase.database().ref('/status/' + uid);
 
@@ -149,11 +123,8 @@ export class SDashboard {
       last_changed: this.firebase.database.ServerValue.TIMESTAMP,
     };
 
-    // [END_EXCLUDE]
     var userStatusFirestoreRef = this.firebase.firestore().doc('/status/' + uid);
 
-    // Firestore uses a different server timestamp value, so we'll 
-    // create two more constants for Firestore state.
     var isOfflineForFirestore = {
       state: 'offline',
       last_changed: this.firebase.firestore.FieldValue.serverTimestamp(),
@@ -166,25 +137,18 @@ export class SDashboard {
 
     this.firebase.database().ref('.info/connected').on('value', function (snapshot) {
       if (snapshot.val() == false) {
-        // Instead of simply returning, we'll also set Firestore's state
-        // to 'offline'. This ensures that our Firestore cache is aware
-        // of the switch to 'offline.'
         userStatusFirestoreRef.set(isOfflineForFirestore);
         return;
       };
 
       userStatusDatabaseRef.onDisconnect().set(isOfflineForDatabase).then(function () {
         userStatusDatabaseRef.set(isOnlineForDatabase);
-
-        // We'll also add Firestore set here for when we come online.
         userStatusFirestoreRef.set(isOnlineForFirestore);
       });
     });
-    // [END rtdb_and_local_fs_presence]
   }
 
   fs_listen_online() {
-    // [START fs_onsnapshot_online]
     this.firebase.firestore().collection('status')
       .where('state', '==', 'online')
       .onSnapshot(function (snapshot) {
@@ -192,20 +156,15 @@ export class SDashboard {
           if (change.type === 'added') {
             var msg = 'User ' + change.doc.id + ' is online.';
             console.log(msg);
-            // [START_EXCLUDE]
-            this.history.push(msg);
-            // [END_EXCLUDE]
+            this.msghistory.push(msg);
           }
           if (change.type === 'removed') {
             var msg = 'User ' + change.doc.id + ' is offline.';
             console.log(msg);
-            // [START_EXCLUDE]
-            this.history.push(msg);
-            // [END_EXCLUDE]
+            this.msghistory.push(msg);
           }
         });
       });
-    // [END fs_onsnapshot_online]
   }
 
   @Listen('joinCompleted')
@@ -242,7 +201,7 @@ export class SDashboard {
               :
               <s-login firebase={this.firebase}></s-login>
             }
-            {this.history}
+            {this.msghistory}
           </div>
         }
       </Host>
