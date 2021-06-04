3fb574d8d524209bbc5932daafbb98bb8b3c05ad
Alan <bigalan09@gmail.com>
2021-06-04 07:34:49 +0000

    stuff

--- scrum/src/components/s-avatar/s-avatar.tsx
+++ scrum/src/components/s-avatar/s-avatar.tsx
@@ -3,7 +3,7 @@ import { Component, h, Host, Prop, Element } from '@stencil/core';
 @Component({
   tag: 's-avatar',
   styleUrl: 's-avatar.css',
-  shadow: true,
+  shadow: false,
 })
 export class SAvatar {
   @Element() hostElement: HTMLElement;
@@ -11,16 +11,22 @@ export class SAvatar {
   @Prop() online: boolean = false;
 
   render() {
-    let onlineClass = 'status-circle';
-    if (this.online) {onlineClass += 'status-circle--online'; }
     return (
         <Host>
         <div class='icon-container'>
             <img src="https://cdn2.iconfinder.com/data/icons/flatfaces-everyday-people-square/128/beard_male_man_face_avatar-512.png" />
-            <div class={onlineClass}>
+            <div class={this.getCssClassMap()}>
             </div>
         </div>
         </Host>
     );
+  }
+
+  getCssClassMap() {
+    const classes = ['status-circle'];
+    if (this.online == true) {
+      classes.push('status-circle--online');
+    }
+    return classes.join(' ');
   }
 }
